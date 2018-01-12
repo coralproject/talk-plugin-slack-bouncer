28c58c9b3dd7368c09b4aaa27a4882db628a42ac
Wyatt Johnson <wyattjoh@gmail.com>
2018-01-11 20:34:58 -0700

    upgraded linter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pkg = require('./package.json');
 const debug = require('debug')('talk-plugin-slack-bouncer');
 const Joi = require('joi');
-const {get} = require('lodash');
+const { get } = require('lodash');
 const fetch = require('node-fetch');
 const authorization = require('middleware/authorization');
 const {
@@ -11,11 +11,15 @@ const {
 } = process.env;
 
 if (!TALK_SLACK_BOUNCER_URL || !TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN) {
-  console.warn('talk-plugin-slack-bouncer: will work without TALK_SLACK_BOUNCER_URL and TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN is provided');
+  console.warn(
+    'talk-plugin-slack-bouncer: will work without TALK_SLACK_BOUNCER_URL and TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN is provided'
+  );
 }
 
 if (!TALK_SLACK_BOUNCER_AUTH_TOKEN) {
-  console.warn('talk-plugin-slack-bouncer: will not send comments unless TALK_SLACK_BOUNCER_AUTH_TOKEN is provided');
+  console.warn(
+    'talk-plugin-slack-bouncer: will not send comments unless TALK_SLACK_BOUNCER_AUTH_TOKEN is provided'
+  );
 }
 
 const sendCommentID = (id, source) => {
@@ -29,8 +33,8 @@ const sendCommentID = (id, source) => {
       'X-Handshake-Token': TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN,
       'Content-Type': 'application/json',
       'User-Agent': `talk-plugin-slack-bouncer/${pkg.version}`,
-      'Authorization': TALK_SLACK_BOUNCER_AUTH_TOKEN
-    }
+      Authorization: TALK_SLACK_BOUNCER_AUTH_TOKEN,
+    },
   };
 
   // Send off the request to the bouncer url.
@@ -48,7 +52,7 @@ module.exports = {
           }
 
           // Extract the item_id, item_type from the flag.
-          const {item_id, item_type} = flag;
+          const { item_id, item_type } = flag;
 
           // If the flag isn't against a comment, then we can't do anything
           // here.
@@ -66,7 +70,7 @@ module.exports = {
 
           // Only emit that a comment was flagged if this was the first flag.
           const flags = get(comment, 'action_counts.flag') || 0;
-          if (flags !== 0)  {
+          if (flags !== 0) {
             return res;
           }
 
@@ -74,13 +78,13 @@ module.exports = {
 
           // Only emit that a comment was flagged if the comment did not already
           // come with a REJECTED, PREMOD, or SYSTEM_WITHHELD status's.
-          const hasSubmittedAlready = !status_history.every(({type}) => {
+          const hasSubmittedAlready = !status_history.every(({ type }) => {
             switch (type) {
-            case 'ACCEPTED':
-            case 'NONE':
-              return true;
-            default:
-              return false;
+              case 'ACCEPTED':
+              case 'NONE':
+                return true;
+              default:
+                return false;
             }
           });
           if (hasSubmittedAlready) {
@@ -102,7 +106,11 @@ module.exports = {
       },
       createComment: {
         post: async (obj, args, ctx, info, res) => {
-          if (!TALK_SLACK_BOUNCER_AUTH_TOKEN || !TALK_SLACK_BOUNCER_URL || !TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN) {
+          if (
+            !TALK_SLACK_BOUNCER_AUTH_TOKEN ||
+            !TALK_SLACK_BOUNCER_URL ||
+            !TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN
+          ) {
             return res;
           }
 
@@ -128,78 +136,98 @@ module.exports = {
     }
 
     // Handle to provide localized strings relative to this installation.
-    router.post('/api/slack-bouncer/translate', authorization.needed('ADMIN', 'MODERATOR'), async (req, res, next) => {
-      const {value: body, error: err} = Joi.validate(req.body, Joi.object().keys({
-        key: Joi.string().required(),
-        replacements: Joi.array().default([])
-      })
-        .optionalKeys('replacements'), {
-        stripUnknown: true,
-        convert: true,
-        presence: 'required',
-      });
-      if (err) {
-        return res.status(400).end();
-      }
-
-      const {key, replacements} = body;
-
-      // Perform the translation.
-      const translation = res.locals.t(key, ...replacements);
-
-      // Return the response.
-      res.format({
-        'text/plain': () => {
-          res.send(translation);
-        },
-        'application/json': () => {
-          res.send({translation});
-        },
-        'default': () => {
-          res.status(406).send('Not Acceptable');
+    router.post(
+      '/api/slack-bouncer/translate',
+      authorization.needed('ADMIN', 'MODERATOR'),
+      async (req, res, next) => {
+        const { value: body, error: err } = Joi.validate(
+          req.body,
+          Joi.object()
+            .keys({
+              key: Joi.string().required(),
+              replacements: Joi.array().default([]),
+            })
+            .optionalKeys('replacements'),
+          {
+            stripUnknown: true,
+            convert: true,
+            presence: 'required',
+          }
+        );
+        if (err) {
+          return res.status(400).end();
         }
-      });
-    });
+
+        const { key, replacements } = body;
+
+        // Perform the translation.
+        const translation = res.locals.t(key, ...replacements);
+
+        // Return the response.
+        res.format({
+          'text/plain': () => {
+            res.send(translation);
+          },
+          'application/json': () => {
+            res.send({ translation });
+          },
+          default: () => {
+            res.status(406).send('Not Acceptable');
+          },
+        });
+      }
+    );
 
     // Handle to verify that the application is properly setup for the slack
     // bouncer features.
-    router.post('/api/slack-bouncer/test', authorization.needed('ADMIN', 'MODERATOR'), (req, res, next) => {
-      const {value: body, error: err} = Joi.validate(req.body, Joi.object().keys({
-        challenge: Joi.string().required(),
-        handshake_token: Joi.string().required(),
-        injestion_url: Joi.string().required(),
-      }), {
-        stripUnknown: true,
-        convert: false,
-        presence: 'required',
-      });
-      if (err) {
-        return res.status(400).end();
+    router.post(
+      '/api/slack-bouncer/test',
+      authorization.needed('ADMIN', 'MODERATOR'),
+      (req, res, next) => {
+        const { value: body, error: err } = Joi.validate(
+          req.body,
+          Joi.object().keys({
+            challenge: Joi.string().required(),
+            handshake_token: Joi.string().required(),
+            injestion_url: Joi.string().required(),
+          }),
+          {
+            stripUnknown: true,
+            convert: false,
+            presence: 'required',
+          }
+        );
+        if (err) {
+          return res.status(400).end();
+        }
+
+        // Don't validate any tests if the auth token is set on the application.
+        if (
+          TALK_SLACK_BOUNCER_AUTH_TOKEN &&
+          TALK_SLACK_BOUNCER_AUTH_TOKEN.length > 0
+        ) {
+          return res.status(400).end();
+        }
+
+        const { challenge, handshake_token, injestion_url } = body;
+
+        // Check that the handshake matches.
+        if (handshake_token !== TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN) {
+          return res.status(400).end();
+        }
+
+        // Check that the injestion url matches.
+        if (injestion_url !== TALK_SLACK_BOUNCER_URL) {
+          return res.status(400).end();
+        }
+
+        // Looks like all the parameters match, respond with the challenge.
+        return res.status(202).json({
+          talk_version: get(this, 'pkg.version', '3.0.0'),
+          challenge,
+          client_version: pkg.version,
+        });
       }
-
-      // Don't validate any tests if the auth token is set on the application.
-      if (TALK_SLACK_BOUNCER_AUTH_TOKEN && TALK_SLACK_BOUNCER_AUTH_TOKEN.length > 0) {
-        return res.status(400).end();
-      }
-
-      const {challenge, handshake_token, injestion_url} = body;
-
-      // Check that the handshake matches.
-      if (handshake_token !== TALK_SLACK_BOUNCER_HANDSHAKE_TOKEN) {
-        return res.status(400).end();
-      }
-
-      // Check that the injestion url matches.
-      if (injestion_url !== TALK_SLACK_BOUNCER_URL) {
-        return res.status(400).end();
-      }
-
-      // Looks like all the parameters match, respond with the challenge.
-      return res.status(202).json({
-        talk_version: get(this, 'pkg.version', '3.0.0'),
-        challenge,
-        client_version: pkg.version,
-      });
-    });
-  }
+    );
+  },
 };
